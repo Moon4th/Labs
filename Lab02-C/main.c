bf68ec8f7580c62d8543e764c5b5a39eb104f201
Moon4th <32035576+Moon4th@users.noreply.github.com>
2017-10-15 19:12:51 +0300

    Add files via upload

--- Lab02-C/main.c
+++ Lab02-C/main.c
@@ -6,9 +6,9 @@ int main()
     double a, x, G, F, Y;
     int n;
     printf("select the formula\n");
-    printf("Formula 1: G = 4*(-4 * pow(a, 2) +a * x + 5 * pow(x, 2)) / (-20 * pow(a, 2) + 28 * a * x+ 3 * pow(x, 2)\n");
-    printf("Formula 2: F = atan (24 * pow(a,2) - 25 * a * x + 6 * pow (x,2)\n");
-    printf("Formula 3: Y= log (2 * pow(a,2) - 7 * a * x + 6 * pow (x,2) + 1)\n");
+    printf("Formula 1: G = 4*(-4*pow(a,2)+a*x+5*pow(x,2))/-20*pow(a,2)+28*a*x+3*pow(x,2)\n");
+    printf("Formula 2: F = atan(24*pow(a,2)-25*a*x+6*pow(x,2)\n");
+    printf("Formula 3: Y= log(2*pow(a,2)-7*a*x+6*pow(x,2)+1)\n");
     scanf("%d",&n);
     printf("Enter x: ");
     scanf("%lf", &x);
@@ -17,7 +17,7 @@ int main()
 
     switch (n) {
         case 1:
-            G = 4 * (-4 * pow(a, 2) + a * x + 5 * pow(x, 2)) / (-20 * pow(a, 2) + 28 * a * x + 3 * pow(x, 2));
+            G = 4 * (-4 * pow(a, 2) + a * x + 5 * pow(x, 2)) / -20 * pow(a, 2) + 28 * a * x + 3 * pow(x, 2);
             printf("Result: ");
             printf("%lf", G);
             break;
