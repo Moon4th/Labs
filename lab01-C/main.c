e55dae1dcee609761a65d8e9332803db339c8d26
Moon4th <32035576+Moon4th@users.noreply.github.com>
2017-10-15 19:13:24 +0300

    Add files via upload

--- lab01-C/main.c
+++ lab01-C/main.c
@@ -9,23 +9,23 @@ int main()
     scanf("%lf", &a);
     printf("Enter x: ");
     scanf("%lf", &x);
-    G = 4*(-4 * pow(a, 2) +a * x + 5 * pow(x, 2)) / (-20 * pow(a, 2) + 28 * a * x+ 3 * pow(x, 2));
+    G = 4*(-4*pow(a,2)+a*x+5*pow(x,2))/-20*pow(a,2)+28*a*x+3*pow(x,2);
     printf("Result: ");
     printf("%lf", G);
 
-    printf("\n\n Enter a: ");
+    printf("\nEnter a: ");
     scanf("%lf", &a);
     printf("Enter x: ");
     scanf("%lf", &x);
-    F = atan (24 * pow(a,2) - 25 * a * x + 6 * pow (x,2));
+    F = atan(24*pow(a,2)-25*a*x+6*pow(x,2));
     printf("Result: ");
     printf("%lf", F);
 
-    printf("\n\n Enter a: ");
+    printf("\nEnter a: ");
     scanf("%lf", &a);
     printf("Enter x: ");
     scanf("%lf", &x);
-    Y=log(2 * pow(a,2) - 7 * a * x + 6 * pow (x,2) + 1);
+    Y=log(2*pow(a,2)-7*a*x+6*pow(x,2)+1);
     printf("Result: ");
     printf("%lf", Y);
 
