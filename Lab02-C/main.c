1327037bb3d0240c23b60fca7b8d7d5f3382212f
Moon4th <32035576+Moon4th@users.noreply.github.com>
2017-10-18 19:01:00 +0300

    Add files via upload

--- Lab02-C/main.c
+++ Lab02-C/main.c
@@ -14,26 +14,30 @@ int main()
     scanf("%lf", &x);
     printf("Enter a:");
     scanf("%lf",&a);
+        switch (n) {
+            case 1:
+                G = 4 * (-4 * pow(a, 2) + a * x + 5 * pow(x, 2)) / -20 * pow(a, 2) + 28 * a * x + 3 * pow(x, 2);
+                printf("Result: ");
+                printf("%lf", G);
+                break;
+            case 2:
+                F = atan(24 * pow(a, 2) - 25 * a * x + 6 * pow(x, 2));
+                printf("Result: ");
+                printf("%lf", F);
+                break;
+            case 3:
+                if (x < 0)
+                {
+                    printf("Error");
+                    return 0;
+                }
+                Y = log(2 * pow(a, 2) - 7 * a * x + 6 * pow(x, 2) + 1);
+                printf("Result: ");
+                printf("%lf", Y);
+                break;
 
-    switch (n) {
-        case 1:
-            G = 4 * (-4 * pow(a, 2) + a * x + 5 * pow(x, 2)) / -20 * pow(a, 2) + 28 * a * x + 3 * pow(x, 2);
-            printf("Result: ");
-            printf("%lf", G);
-            break;
-        case 2:
-            F = atan(24 * pow(a, 2) - 25 * a * x + 6 * pow(x, 2));
-            printf("Result: ");
-            printf("%lf", F);
-            break;
-        case 3:
-            Y = log(2 * pow(a, 2) - 7 * a * x + 6 * pow(x, 2) + 1);
-            printf("Result: ");
-            printf("%lf", Y);
-            break;
-
-        default:
-            printf("Error");
-    }
+            default:
+                printf("Error");
+        }
     return 0;
 }
