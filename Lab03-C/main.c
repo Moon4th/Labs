496f94965843d7f7adf9760a10c6bcd0c7701b9a
Moon4th <32035576+Moon4th@users.noreply.github.com>
2017-10-28 22:14:25 +0300

    Add files via upload

--- Lab03-C/main.c
+++ Lab03-C/main.c
@@ -3,14 +3,14 @@
 int main() {
     double a, x, G, F, Y,xmax,xmin;
     int n,steps;
-    do {
+    do{
         printf("Formula 1: G = 4*(-4 * pow(a, 2) +a * x + 5 * pow(x, 2)) / -20 * pow(a, 2) + 28 * a * x+ 3 * pow(x, 2)\n");
         printf("Formula 2: F = atan (24 * pow(a,2) - 25 * a * x + 6 * pow (x,2)\n");
         printf("Formula 3: Y= log (2 * pow(a,2) - 7 * a * x + 6 * pow (x,2) + 1)\n");
         printf("Enter 0 to quit\n");
         printf("Select the formula:\n");
         scanf("%d", &n);
-        if (n == 0) {
+        if (n==0){
             return 0;
         }
         while ((n < 0) || (n > 3)) {
@@ -19,40 +19,39 @@ int main() {
             scanf("%d", &n);
         }
         printf("Enter minimum value of x:");
-        scanf("%lf", &xmin);
+        scanf("%lf",&xmin);
         printf("Enter maximum value of x:");
-        scanf("%lf", &xmax);
-        for (;;) {
-            printf("Enter x (from %lf to %lf):", xmin, xmax);
-            scanf("%lf", &x);
-            if (x < xmin || x > xmax) {
-                printf("x must be within the scope of the definition\nTry again\n");
-                continue;
-            } else break;
+        scanf("%lf",&xmax);
+        while (xmin>=xmax){
+            printf("Maximum value of x should be grater than the minimum\n");
+            printf("Minimum value = %lf\n",xmin);
+            printf("Enter maximum value of x again:");
+            scanf("%lf",&xmax);
         }
         printf("Enter a (any value):");
         scanf("%lf", &a);
         printf("Enter the number of steps");
-        scanf("%d", &steps);
+        scanf("%d",&steps);
         switch (n) { //Выбор формулы
             case 1:
-                for (steps = steps; steps > 0; steps--) {
+                for(x=xmin;x<=xmax;x += (xmax - xmin) / (steps - 1)){
                     G = 4 * (-4 * pow(a, 2) + a * x + 5 * pow(x, 2)) / -20 * pow(a, 2) + 28 * a * x + 3 * pow(x, 2);
                     printf("%lf\n", G);
                 }
                 break;
             case 2:
-                for (steps = steps; steps >= 0; steps--) {
+                for(x=xmin;x<=xmax;x += (xmax - xmin) / (steps - 1)){
                     F = atan(24 * pow(a, 2) - 25 * a * x + 6 * pow(x, 2));
                     printf("%lf\n", F);
                 }
                 break;
             case 3:
-                if (x < 0) {
-                    printf("Error");
-                    return 0;
-                }
-                for (steps = steps; steps >= 0; steps--) {
+                for(x=xmin;x<=xmax;x += (xmax - xmin) / (steps - 1)){
+                    if (x < 0)
+                    {
+                        printf("Error");
+                        return 0;
+                    }
                     Y = log(2 * pow(a, 2) - 7 * a * x + 6 * pow(x, 2) + 1);
                     printf("%lf\n", Y);
                 }
