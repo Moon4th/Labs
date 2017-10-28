0d2702ca4ea85ec4f918a8fb65c0d708abc00e68
Moon4th <32035576+Moon4th@users.noreply.github.com>
2017-10-28 22:25:58 +0300

    Add files via upload

--- Lab03-C/main.c
+++ Lab03-C/main.c
@@ -2,27 +2,27 @@
 #include<math.h>
 int main() {
     double a, x, G, F, Y,xmax,xmin;
-    int n,steps;
+    int formula,steps;
     do{
         printf("Formula 1: G = 4*(-4 * pow(a, 2) +a * x + 5 * pow(x, 2)) / -20 * pow(a, 2) + 28 * a * x+ 3 * pow(x, 2)\n");
         printf("Formula 2: F = atan (24 * pow(a,2) - 25 * a * x + 6 * pow (x,2)\n");
         printf("Formula 3: Y= log (2 * pow(a,2) - 7 * a * x + 6 * pow (x,2) + 1)\n");
         printf("Enter 0 to quit\n");
         printf("Select the formula:\n");
-        scanf("%d", &n);
-        if (n==0){
+        scanf("%d", &formula);//Выбор формулы для дальнейших вычислений
+        if (formula==0){
             return 0;
         }
-        while ((n < 0) || (n > 3)) {
+        while ((formula < 0) || (formula > 3)) { //Проверка правильности выбора формулы
             printf("Invalid value, try again\n");
             printf("Select the formula:");
-            scanf("%d", &n);
+            scanf("%d", &formula);
         }
-        printf("Enter minimum value of x:");
+        printf("Enter minimum value of x:");//Ввод минимального значения x
         scanf("%lf",&xmin);
-        printf("Enter maximum value of x:");
+        printf("Enter maximum value of x:");//Ввод максимального значения x
         scanf("%lf",&xmax);
-        while (xmin>=xmax){
+        while (xmin>=xmax){//Цикл проверяющий в правильночти выбор границ x
             printf("Maximum value of x should be grater than the minimum\n");
             printf("Minimum value = %lf\n",xmin);
             printf("Enter maximum value of x again:");
@@ -30,11 +30,11 @@ int main() {
         }
         printf("Enter a (any value):");
         scanf("%lf", &a);
-        printf("Enter the number of steps");
+        printf("Enter the number of steps:");//Ввод количества шагов вычисления функции
         scanf("%d",&steps);
-        switch (n) { //Выбор формулы
+        switch (formula) { //Переключение на одну из 3 формулы в зависимости от начального выбора
             case 1:
-                for(x=xmin;x<=xmax;x += (xmax - xmin) / (steps - 1)){
+                for(x=xmin;x<=xmax;x += (xmax - xmin) / (steps - 1)){//Цикл вычисления формулы с учетом данных введенных выше
                     G = 4 * (-4 * pow(a, 2) + a * x + 5 * pow(x, 2)) / -20 * pow(a, 2) + 28 * a * x + 3 * pow(x, 2);
                     printf("%lf\n", G);
                 }
@@ -57,6 +57,6 @@ int main() {
                 }
                 break;
         }
-    }while(n!=0);
+    }while(formula!=0);//Работа программы будет повоторятся до тех пор, пока пользователь не захочет выйти
     return 0;
 }
