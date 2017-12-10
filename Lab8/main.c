4da3ed6442434fe29255d7dc310a1d04aca8e327
Moon4th <32035576+Moon4th@users.noreply.github.com>
2017-12-10 19:33:56 +0300

    Add files via upload

--- Lab8/main.c
+++ Lab8/main.c
@@ -2,59 +2,57 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
-#define size 100
-FILE *file, *coordinates_x, *coordinates_y;
+#define size 5000
+FILE *t, *coordinates_x, *coordinates_y;
 
-void coordinate(int array[3][size]);
-int count(int dot,int array[3][size], int radius, int c);
+void coordinates(int *x,int *y, int limit);
 
 int main() {
 
     srand (time(NULL));
-    double start = clock(),stop;
-    int coordinates[3][size], dot, R, counter = 0;
+    double start,stop, R;
+    int x[size],y[size], dot, counter = 0, i;
 
-    coordinate(coordinates);
+    t = fopen("C:\\Users\\Nikita\\ClionProjects\\lab8\\t.txt","w+");
 
-    dot = 1 + rand() % 99;
+    for ( i = 0; i < size; i += 10) {
+
+        start = clock();
+        coordinates(x, y, i);
+        stop = clock();
+        fprintf(t, "%lf\n", (stop - start) / CLOCKS_PER_SEC);
+    }
+
+    fclose (t);
+
+    dot = 1 + rand() % size;
 
     printf("Enter the radius of the circle:");
-    scanf("%d", &R);
+    scanf("%lf", &R);
 
     coordinates_x = fopen("C:\\Users\\Nikita\\ClionProjects\\lab8\\coordinates_x.txt","w+");
     coordinates_y = fopen("C:\\Users\\Nikita\\ClionProjects\\lab8\\coordinates_y.txt","w+");
 
-    counter = count(dot,coordinates,R,counter);
+    for (i = 1; i < size;i++) {
+        if (pow((x[i] - x[dot]), 2) + pow((y[i] - y[dot]), 2) <= pow(R, 2)){
+            fprintf(coordinates_x, "%d\n",x[i]);
+            fprintf(coordinates_y, "%d\n",y[i]);
+            counter++;
+        }
+    }
 
     fclose (coordinates_x);
     fclose (coordinates_y);
 
     printf("Amount of points - %d\n",counter);
-    stop = clock();
-    printf("Lead time - %lf\n", (stop - start) / CLOCKS_PER_SEC);
-
-    file = fopen("C:\\Users\\Nikita\\ClionProjects\\lab8\\text.txt","w+");
-    fprintf(file, "%lf", (stop - start) / CLOCKS_PER_SEC);
-    fclose (file);
 
     return 0;
 }
 
-void coordinate(int array[3][size]){
+void coordinates(int *x, int *y, int limit){
     int i;
-    for(i = 1; i < size; i++) {
-        array[1][i] = -5 + rand() % 10;
-        array[2][i] = -5 + rand() % 10;
+    for(i = 0; i <= limit; i++) {
+        x[i] = -5 + rand() % 10;
+        y[i] = -5 + rand() % 10;
     }
-}
-int count(int dot,int array[3][size], int radius,int c){
-    int i;
-    for (i = 1; i < size;i++) {
-       if (pow((array[1][i] - array[1][dot]), 2) + pow((array[2][i] - array[2][dot]), 2) <= pow(radius, 2)){
-           fprintf(coordinates_x, "%d\n",array[1][i]);
-           fprintf(coordinates_y, "%d\n",array[2][i]);
-           c++;
-       }
-    }
-    return(c);
 }
