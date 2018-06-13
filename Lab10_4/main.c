7bce3dc66701b09b77082e673a4cfc73cd91c881
Moon4th <32035576+Moon4th@users.noreply.github.com>
2018-06-13 17:30:11 +0300

    Add files via upload

--- Lab10_4/main.c
+++ Lab10_4/main.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
-{
-    int *a;
-    int i;
-    a = (int*)malloc(10 * sizeof(int));
-    for (i = 0; i <= 20; i++)
-        printf("%d %d\n", i, i*i);
-    free(a);
+int main() {
+
+    srand(time(NULL));
+    int size = 0, i;
+    int *values = NULL, *square_values = NULL;
+
+    printf("Enter the size of the array:");
+    scanf("%d",&size);
+    values = (int*)malloc(size * sizeof(int));
+    square_values = (int*)malloc(size * sizeof(int));
+
+    for (i = 0;i<size;i++){
+        values[i] = 1 + rand() % 100;
+    }
+
+    for (i = 0;i<size;i++){
+        printf("%d | ", values[i]);
+        square_values[i] = values[i] * values[i];
+        printf("%d\n", square_values[i]);
+    }
+
+    free(values);
+    free(square_values);
+
     return 0;
 }
