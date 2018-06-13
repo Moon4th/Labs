3c2b09bc9ea6b6f4943f02c1ab086d4ee57fbf19
Moon4th <32035576+Moon4th@users.noreply.github.com>
2018-06-13 09:56:01 +0300

    Add files via upload

--- Lab4/main.c
+++ Lab4/main.c
@@ -1,106 +1,30 @@
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
-#define size 100
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    double a, x, G, F, Y, x_max = 0, x_min = 0, minvalue, maxvalue, denominator, results[size];
-    int choice = 1, i , steps;
 
-    while (choice != 0){
+    srand(time(NULL));
+    int size = 0, i;
+    int *values = NULL, *square_values = NULL;
 
-        printf("Formula 1: G = 4*(-4 * pow(a, 2) +a * x + 5 * pow(x, 2)) / -20 * pow(a, 2) + 28 * a * x+ 3 * pow(x, 2)\n");
-        printf("Formula 2: F = atan (24 * pow(a,2) - 25 * a * x + 6 * pow (x,2)\n");
-        printf("Formula 3: Y= log (2 * pow(a,2) - 7 * a * x + 6 * pow (x,2) + 1)\n");
-        printf("Enter 0 to quit\n");
-        printf("Select the formula:\n");
-        scanf("%d", &choice);
-        while ((choice < 0) || (choice > 3)) {
-            printf("Invalid value, try again\n");
-            printf("Select the formula:");
-            scanf("%d", &choice);
-        }
-        if (choice == 0) {
-            break;
-        }
+    printf("Enter the size of the array:");
+    scanf("%d",&size);
+    values = (int*)malloc(size * sizeof(int));
+    square_values = (int*)malloc(size * sizeof(int));
 
-        printf("Enter minimum value of x:");
-        scanf("%lf",&x_min);
-        printf("Enter maximum value of x:");
-        scanf("%lf",&x_max);
-        while (x_min>=x_max) {
-            printf("Maximum value of x should be grater than the minimum\n");
-            printf("Minimum value = %lf\n", x_min);
-            printf("Enter maximum value of x again:");
-            scanf("%lf", &x_max);
-        }
-
-        printf("Enter a:");
-        scanf("%lf",&a);
-
-        printf("Enter the number of steps,not more than %d:", size);
-        scanf("%d",&steps);
-        while(steps<=0){//Проверка на корректный ввод кол-ва шагов
-            printf("Error,the number of steps can not be less than or equal to 0\n");
-            printf("Enter the number of steps,not more than %d:",size);
-            scanf("%d",&steps);
-        }
-        if (steps>=size){
-            printf ("Error,too many steps\n");
-            break;
-        }
-
-        switch (choice) { //Переключение на одну из 3 формулы в зависимости от начального выбора
-            case 1:
-                for(x=x_min,i = 0;x<=x_max, i<steps;x += (x_max - x_min) / (steps - 1),i++){
-                    denominator = -20 * pow(a, 2) + 28 * a * x + 3 * pow(x, 2);
-                    if (denominator == 0){
-                        printf("The input values do not belong to the domain of the function definition");
-                        continue;
-                    }
-                    G = 4 * ( - 4 * pow (a ,2) + a * x + 5 * pow (x, 2)) / denominator;
-                    results[i]=G;
-                    printf("x =%lf\tG =%lf\n",x,G);
-                }
-                break;
-
-            case 2:
-                for(x=x_min,i = 0;x<=x_max, i<steps;x += (x_max - x_min) / (steps - 1),i++){
-                    F = atan(24 * pow(a, 2) - 25 * a * x + 6 * pow(x, 2));
-                    results[i]=F;
-                    printf("x =%lf\tG =%lf\n",x,F);
-                }
-                break;
-
-            case 3:
-                for(x=x_min,i = 0;x<=x_max, i<steps;x += (x_max - x_min) / (steps - 1),i++){
-                    if (x < 0 || a < 0) {
-                        printf("The input values do not belong to the domain of the function definition");
-                        continue;
-                    }
-                    Y = log(2 * pow(a, 2) - 7 * a * x + 6 * pow(x, 2) + 1);
-                    results[i]=Y;
-                    printf("x =%lf\tG =%lf\n",x,Y);
-                }
-                break;
-
-            default:
-                break;
-        }
-
-        for (maxvalue=0,i=0,minvalue=results[0];i<steps;i++) {
-            if (results[i]>maxvalue){
-                maxvalue=results[i];
-            }
-            if (results[i]<minvalue){
-                minvalue=results[i];
-            }
-        }
-        printf ("Minimum value=%lf\n",minvalue);
-        printf ("Maximum value=%lf\n",maxvalue);
-
-        memset (results,0,size);
+    for (i = 0;i<size;i++){
+        values[i] = 1 + rand() % 100;
     }
-    printf ("Shutdown...");
+
+    for (i = 0;i<size;i++){
+        printf("%d | ", values[i]);
+        square_values[i] = values[i] * values[i];
+        printf("%d\n", square_values[i]);
+    }
+
+    free(values);
+    free(square_values);
+
     return 0;
 }
