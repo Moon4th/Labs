5f392d418c017659355adf4d0f7bdedeb9a248c9
Moon4th <32035576+Moon4th@users.noreply.github.com>
2018-06-13 09:55:45 +0300

    Add files via upload

--- Lab3/main.c
+++ Lab3/main.c
@@ -1,86 +1,24 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <string.h>
+#define size 64
+
 int main() {
-    double a, x, G, F, Y, x_max = 0, x_min = 0, denominator;
-    int choice = 1, steps;
 
-        while (choice != 0) {
+    int i, q;
+    char sequence[size] = {0}, result[size] = {0}, buffer[size] = {0}, vowels[] = {'A', 'a', 'E', 'e', 'Y', 'y', 'I', 'i', 'U', 'u', 'O', 'o'};
 
-            printf("Formula 1: G = 4*(-4 * pow(a, 2) +a * x + 5 * pow(x, 2)) / -20 * pow(a, 2) + 28 * a * x+ 3 * pow(x, 2)\n");
-            printf("Formula 2: F = atan (24 * pow(a,2) - 25 * a * x + 6 * pow (x,2)\n");
-            printf("Formula 3: Y= log (2 * pow(a,2) - 7 * a * x + 6 * pow (x,2) + 1)\n");
-            printf("Enter 0 to quit\n");
-            printf("Select what tou want:\n");
-            scanf("%d", &choice);
+    printf("Enter the sequence:\n");
+    scanf("%s", sequence);
 
-            while ((choice < 0) || (choice > 3)) {
-                printf("Invalid value, try again\n");
-                printf("Select what tou want:");
-                scanf("%d", &choice);
-            }
-            if (choice == 0) {
-                break;
-            }
-
-            printf("Enter minimum value of x:");
-            scanf("%lf", &x_min);
-            printf("Enter maximum value of x:");
-            scanf("%lf", &x_min);
-            while (x_min >= x_max) {
-                printf("Maximum value of x should be grater than the minimum\n");
-                printf("Minimum value = %lf\n", x_min);
-                printf("Enter maximum value of x again:");
-                scanf("%lf", &x_max);
-            }
-
-            printf("Enter a :");
-            scanf("%lf", &a);
-
-            printf("Enter the number of steps:");
-            scanf("%d", &steps);
-            while (steps <= 0) {
-                printf("Error,the number of steps can not be less than or equal to 0\n");
-                printf("Enter the number of steps:");
-                scanf("%d", &steps);
-            }
-
-            switch (choice) {
-                case 1:
-                    for (x = x_min; x <= x_max;) {
-                        denominator = -20 * pow(a, 2) + 28 * a * x + 3 * pow(x, 2);
-                        if (denominator == 0) {
-                            printf("The input values do not belong to the domain of the function definition");
-                            continue;
-                        }
-                        G = 4 * (-4 * pow(a, 2) + a * x + 5 * pow(x, 2)) / denominator;
-                        printf("G =%lf\n", G);
-                        x += (x_max - x_min) / (steps - 1);
-                    }
-                    break;
-
-                case 2:
-                    for (x = x_min; x <= x_max; ) {
-                        F = atan(24 * pow(a, 2) - 25 * a * x + 6 * pow(x, 2));
-                        printf("F =%lf\n", F);
-                        x += (x_max - x_min) / (steps - 1);
-                    }
-                    break;
-
-                case 3:
-                    for (x = x_min; x <= x_max;) {
-                        if (x < 0 || a < 0) {
-                            printf("The input values do not belong to the domain of the function definition");
-                        }
-                        Y = log(2 * pow(a, 2) - 7 * a * x + 6 * pow(x, 2) + 1);
-                        printf("Y =%lf\n", Y);
-                        x += (x_max - x_min) / (steps - 1);
-                    }
-                    break;
-
-                default:
-                    break;
+    for (i = 0; i <= strlen(sequence); i++){
+        for(q = 0; q < strlen(vowels); q++){
+            if (sequence[i] == vowels[q]){
+                sprintf(buffer, "%c", sequence[i]);
+                strcat(result, buffer);
             }
         }
-    printf ("Shutdown...");
+    }
+
+    printf("Vowels - %s", result);
     return 0;
 }
